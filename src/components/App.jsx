0bde41d3e34c923201396b98a4c49ea31fc0d84f
K Mak <kumarajiva08@gmail.com>
2017-09-26 23:05:05 +1000

    Header component - Todo List

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import Header from './Header'
+
 class App extends React.Component {
     render() {
-        return <div>Hello world</div>;
+        return <Header />;
     }
 }
 
