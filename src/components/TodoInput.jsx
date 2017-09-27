3df61857c33a12a41b76b20cb204eea9a3d8513a
K Mak <kumarajiva08@gmail.com>
2017-09-27 12:51:38 +1000

    accessing value on text box using refs assigned to them

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 class TodoInput extends React.Component {
     handleSubmit(event) {
         event.preventDefault();
-        console.log('handleSubmit');
+        console.log(this.refs.input.value);
     }
     render() {
         return (
             <form onSubmit={this.handleSubmit.bind(this)}>
-                <input type="text" />
+                <input type="text" ref="input" />
                 <button>Add</button>
             </form>
         );
