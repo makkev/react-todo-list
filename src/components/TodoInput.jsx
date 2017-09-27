38f160b46009df1c7e7bd74caa5d0d93bfab8bf9
K Mak <kumarajiva08@gmail.com>
2017-09-27 12:23:10 +1000

    onSubmit event handler on input text box

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
 class TodoInput extends React.Component {
+    handleSubmit(event) {
+        event.preventDefault();
+        console.log('handleSubmit');
+    }
     render() {
         return (
-            <form>
+            <form onSubmit={this.handleSubmit.bind(this)}>
                 <input type="text" />
                 <button>Add</button>
             </form>
